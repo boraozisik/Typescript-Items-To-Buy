119c433a7572e6f50a67c27aaa62f172e11b9f22
Bora Özışık <ozisikbora@gmail.com>
2022-06-14 14:30:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,51 @@
-import React, {useState} from 'react';
-import './App.css';
-import ItemList from "./components/ItemList"
-import AddItem from "./components/AddItem"
+import React, { useState } from "react";
+import ItemList from "./components/ItemList";
+import AddItem from "./components/AddItem";
+import { Container, Typography } from "@mui/material";
 
 export interface IState {
   items: {
-    name: string,
-    price: number,
-    url: string,
-    description?: string
-  }[]
+    name: string;
+    price: number;
+    url: string;
+    description?: string;
+  }[];
 }
 
-
 function App() {
-
-  const [items,setItems] = useState<IState["items"]>([
+  const [items, setItems] = useState<IState["items"]>([
     {
-    name: "Anoka Desk W567",
-    url: "https://secure.img1-fg.wfcdn.com/im/70268433/compr-r85/1697/169728879/anoka-desk.jpg",
-    price: 209,
-    description: "This clean-lined computer desk brings versatile storage and modern style to your bedroom or dorm room."
-    },  
+      name: "Anoka Desk ",
+      url: "https://secure.img1-fg.wfcdn.com/im/70268433/compr-r85/1697/169728879/anoka-desk.jpg",
+      price: 209,
+      description:
+        "This clean-lined computer desk brings versatile storage and modern style to your bedroom or dorm room.",
+    },
     {
       name: "AR4071 Clean Force",
       url: "https://cdn.arzum.com.tr/ar4071-clean-force-red-cyclone-filter-vacuum-cleaner-red-bagless-vacuum-cleaner-arzum-ar4071-2189-23-K.jpg",
       price: 150,
-      description: "AR4071 Clean Force Red Cyclone Filter Vacuum Cleaner, HIGH PERFORMANCE"
-    },  
+      description:
+        "AR4071 Clean Force Red Cyclone Filter Vacuum Cleaner, HIGH PERFORMANCE",
+    },
     {
       name: "Arva - Axe V3 Shovel",
       url: "https://www.bfgcdn.com/600_600_90/407-0009-0111/arva-axe-v3-shovel-avalanche-shovel.jpg",
       price: 75,
-      description: "The Axe V3 shovel from Arva is a very strong avalanche shovel. It is especially well suited for people looking for a compact and versatile option"
-    },  
-
-
-])
-
+      description:
+        "The Axe V3 shovel from Arva is a very strong avalanche shovel. It is especially well suited for people looking for a compact and versatile option",
+    },
+  ]);
 
   return (
-    <div className="App">
-      <h1>Items To Buy</h1>
+    <Container sx={{ textAlign: "center", marginTop: 2 }}>
+      <Typography variant="h3">Items To Buy</Typography>
+      <hr />
       <ItemList items={items} />
       <AddItem setItems={setItems} items={items} />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
