6f29938e86b04edc475e51f78e7571a52b25d20b
Bora Özışık <ozisikbora@gmail.com>
2022-06-14 14:31:52 +0300

    Update ItemList.tsx

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,42 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Container,
+  List,
+  ListItem,
+  Avatar,
+  Typography,
+  Box,
+} from "@mui/material";
 
 interface IProps {
-    items: {
-      name: string,
-      price: number,
-      url: string,
-      description?: string
-    }[]
-  }
-
-
-const ItemList: React.FC<IProps> = ({items}) => {
-
-
-    const renderItemList = (): JSX.Element[] => {
-         
-        return  items.map((item) => {
-                return(
-                    <li className='itemList' key={item.name}>
-                    <div className='itemListHeader'>
-                            <img className='itemListImage' src={item.url}/>
-                            <h2>{item.name}</h2>                           
-                    </div>
-                    <p>${item.price}</p>
-                    <p className='itemDescription'>{item.description}</p>
-                </li>
-                )
-            })
-
-        
-    }
-
-  return (
-    <ul>
-        {renderItemList()}
-    </ul>
-  )
+  items: {
+    name: string;
+    price: number;
+    url: string;
+    description?: string;
+  }[];
 }
 
-export default ItemList
+const ItemList: React.FC<IProps> = ({ items }) => {
+  const renderItemList = (): JSX.Element[] => {
+    return items.map((item) => {
+      return (
+        <Box
+          sx={{
+            listStyle: "none",
+            display: "flex",
+            alignItems: "center",
+            width: "50rem",
+            margin: "0rem auto",
+            border: " 0.1rem solid #F7DC6F",
+            padding: "1rem",
+            justifyContent: "space-between",
+          }}
+          key={item.name}
+        >
+          <ListItem>
+            <Container sx={{ display: "flex", alignItems: "center" }}>
+              <Avatar
+                sx={{ width: "4rem", height: "4rem", marginRight: "20px" }}
+                src={item.url}
+              />
+              <Typography variant="h5">{item.name}</Typography>
+            </Container>
+          </ListItem>
+          <ListItem>
+            <Typography variant="h6">${item.price}</Typography>
+          </ListItem>
+          <ListItem>
+            <Typography
+              variant="body2"
+              sx={{ width: "70%", textAlign: "left" }}
+            >
+              {item.description}
+            </Typography>
+          </ListItem>
+        </Box>
+      );
+    });
+  };
+
+  return <List sx={{ marginTop: 2 }}>{renderItemList()}</List>;
+};
+
+export default ItemList;
